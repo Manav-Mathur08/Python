ba8b79af280422b1424cca39ae72ff3063e8b441
Manav-Mathur08 <89579610+Manav-Mathur08@users.noreply.github.com>
2021-08-30 19:33:13 +0530

    Delete Extras directory

--- Extras/listempty.py
+++
@@ -1,5 +0,0 @@
-a = list(input("Enter list elements:"))
-if len(a) == 0:
-    print("Empty")
-else:
-    print("Not Empty")
--- Extras/suffix.py
+++
@@ -1,7 +0,0 @@
-a = ['abc','5bc','python','bca']
-c = 0
-for i in a:
-    if i.endswith('bc'):
-        c = c+1
-print(c)
-#print(type(i))
